eb331a0d7b88af96be34aad44a60bdc32f516c6d
zakiul1 <86553317+zakiul1@users.noreply.github.com>
2023-11-24 16:26:19 +0600

    siatex

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl ">
+        <h1 className="text-red-700">Hi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react/src/router.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import App from './App';
+
+const router = createBrowserRouter([
+    {
+        path: '/home',
+        element: <App />
+    },
+]);
+
+export default router
